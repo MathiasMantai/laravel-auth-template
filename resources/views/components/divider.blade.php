8bad535b0bf16dbecc00a36e9406473bea33a722
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-13 08:10:05 +0000

    feat: password change frontend

---
+++ resources/views/components/divider.blade.php
@@ -0,0 +1,4 @@
+<div class="p-2 py-6 w-full">
+    <hr class="w-full" />
+    <h2 class="text-lg font-bold">{{ $slot }}</h2> 
+</div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,7 @@
 
 <main class="flex grow justify-center p-8">
 @if (session('status'))
-<div id="successWindow" class="fixed top-2 right-2 bg-green-400" role="alert">
+<div id="successWindow" class="fixed top-2 right-2 bg-green-400 p-2 px-6 rounded-md shadow-md text-white" role="alert">
     {{ session('status') }}
 </div>
 @elseif (session('error'))
@@ -14,44 +14,77 @@
 </div>
 @endif
 
-<div class="w-[100dvw] sm:w-[40dvw] bg-white rounded-md shadow-md flex flex-col items-center">
-    <h1 class="text-center text-2xl p-4">Profile</h1>
-    <form method="POST" action="{{ route('updateProfile')}}" class="">
-        <table class="table-fixed">
-            <tbody class="w-[100%] p-4">
-                <tr class="w-[100%]">
-                    <td class="">
+<div class="w-[100dvw] lg:w-[40dvw] bg-white rounded-md shadow-md flex flex-col items-center p-4">
+    <h1 class="text-center text-2xl p-4">Profil</h1>
+
+    <x-divider>Profileinstellungen ändern</x-divider>
+
+    <form method="POST" action="{{ route('updateProfile')}}" class="flex flex-col items-center">
+        <table class="table-fixed w-full">
+            <tbody class="p-4">
+                <tr class="">
+                    <td class="text-center">
                         Name
                     </td>
-                    <td class="">
+                    <td class="py-4">
                         <input type="text" name="name" value="{{ $user->name }}" class="w-[100%] p-2 border-2 border-slate-300">
                     </td>
                 </tr>
                 <tr>
-                    <td>
+                    <td class="text-center">
                         Passwort
                     </td>
-                    <td>
-                        <input type="text" name="password" value="{{ $user->password }}">
+                    <td class="py-4">
+                        <input type="text" name="password" value="{{ $user->password }}" class="w-[100%] p-2 border-2 border-slate-300">
                     </td>
                 </tr>
                 <tr>
-                    <td>
+                    <td class="text-center">
                         E-Mail
                     </td>
-                    <td>
-                        <input type="text" name="email" value="{{ $user->email }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="submit" value="Änderungen speichern" class="w-[100%]" />
+                    <td class="py-4">
+                        <input type="text" name="email" value="{{ $user->email }}" class="w-[100%] p-2 border-2 border-slate-300">
                     </td>
                 </tr>
             </tobdy>
         </table>
+        <input type="submit" value="Änderungen speichern" class="w-[50%] bg-sky-500 hover:bg-sky-600 text-white p-2 mt-4 shadow-md cursor-pointer " />
         @csrf
-        
+    </form>
+
+    <x-divider>Passwort ändern</x-divider>
+
+    <form methdo="POST" action="{{ route('updateProfile')}}" class="flex flex-col items-center">
+        <table class="table-fixed w-full">
+            <tbody class="p-4">
+                <tr class="">
+                    <td class="text-center">
+                        Altes Passwort
+                    </td>
+                    <td class="py-4">
+                        <input type="text" name="name"  class="w-[100%] p-2 border-2 border-slate-300">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-center">
+                        Neues Passwort
+                    </td>
+                    <td class="py-4">
+                        <input type="text" name="password" class="w-[100%] p-2 border-2 border-slate-300">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="text-center">
+                        Neues Passwort bestätigen
+                    </td>
+                    <td class="py-4">
+                        <input type="text" name="email"  class="w-[100%] p-2 border-2 border-slate-300">
+                    </td>
+                </tr>
+            </tobdy>
+        </table>
+        <input type="submit" value="Passwort ändern" class="w-[50%] bg-sky-500 hover:bg-sky-600 text-white p-2 mt-4 shadow-md cursor-pointer " />
+        @csrf
     </form>
 </div>
 
